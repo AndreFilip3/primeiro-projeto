923f91fbe6da6bcc9e74a5314969a2d043125945
AndréFilip3 <andrezinzikin@gmail.com>
2025-08-27 20:40:45 -0300

    Adiciona exercício de variáveis e f-string (apresentação pessoal)

---
+++ exercicio1.py
@@ -0,0 +1,9 @@
+
+nome = "André"
+idade = 23
+altura = 1.68
+estudando = True
+
+'''print(f"Meu nome é {nome}, tenho {idade} anos, tenho {altura} de altura,\né {estudando} que estudo Python.")'''
+
+print(f"Olá! Meu nome é {nome}.\nEu tenho {idade} anos e minha altura é {altura}.\nAtualmente estou estudando Python: {estudando}")
