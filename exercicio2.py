9d6a19fff74a8d5866391006133c8d3f4e4af6f7
AndréFilip3 <andrezinzikin@gmail.com>
2025-08-27 21:17:14 -0300

    Adiciona operações matemáticas feitas no exercicio2

---
+++ exercicio2.py
@@ -0,0 +1,11 @@
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+resultado = 0
+
+print("Agora vamos calcular:")
+print(f"{a:.2f} + {b:.2f} = {a + b}\n")
+print(f"{a:.2f} - {b:.2f} = {a - b}\n")
+print(f"{a:.2f} * {b:.2f} = {a * b}\n")
+print(f"{a:.2f} ÷ {b:.2f} = {a / b}")
+
+
